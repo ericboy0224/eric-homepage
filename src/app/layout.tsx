cf855d1fc1166734ba75f08d5f59eb738e9bfc23
Eric Lin <ericboy0224@gmail.com>
2024-08-06 21:56:06 +0800

    chore(app): create app with shadcn

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import './globals.css';
+
+import { Inter as FontSans } from 'next/font/google';
+
+import { cn } from '@/lib/utils';
+
+export const metadata: Metadata = {
+  title: "Eric Homepage",
+  description: "A full-stack developer based in Taipei.",
+};
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={cn(
+        "min-h-screen bg-background font-sans antialiased",
+        fontSans.variable
+      )}>{children}</body>
+    </html>
+  );
+}
